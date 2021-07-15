b8637cb944c6ecff03ca12db36f5cd6a42313ab8
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-07-15 17:51:47 +0300

    Padarytas search vartotojų narystėse

--- sportsclub/fightclub/admin.py
+++ sportsclub/fightclub/admin.py
@@ -3,6 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Group, Membership, UserMembership
 
+class UserMembershipInline(admin.TabularInline):
+    model = UserMembership
+    extra = 0
+
 
 class GroupAdmin(admin.ModelAdmin):
     list_display = ('name', 'week_day', 'start_time')
@@ -10,12 +14,16 @@ class GroupAdmin(admin.ModelAdmin):
 
 class MembershipAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price')
+    inlines = [UserMembershipInline]
 
 
 class UserMembershipAdmin(admin.ModelAdmin):
     list_display = ('member', 'membership', 'display_group')
     # filtravimas pagal narystes ir grupes
     list_filter = ('membership', 'group')
+    search_fields = ('member__username', 'membership__name')
+
+
 
 
 admin.site.register(Group, GroupAdmin)
