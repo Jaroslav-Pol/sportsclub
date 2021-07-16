9bb781272f1669c8cf897468503abb072a615582
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-07-16 15:05:09 +0300

    Padaryta base html, index html, sukurtas redirectas, pagal 4 pask

--- sportsclub/fightclub/views.py
+++ sportsclub/fightclub/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Membership, UserMembership, Group
 
 def index(request):
-    return HttpResponse("Labas baigiamasais darbas")
+    text = "Labas baigiamasais darbas"
+    context = {text, }
+    return render(request, 'fightclub/index.html')
 
 # Create your views here.
