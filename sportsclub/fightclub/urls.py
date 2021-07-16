bb3846b837629676efce3c8df401b1e27b79645d
Jaroslav-Pol <73476503+Jaroslav-Pol@users.noreply.github.com>
2021-07-16 16:58:32 +0300

    Padaryta about, group ir groups puslapiai ir jų nuorodos pagal function based views

--- sportsclub/fightclub/urls.py
+++ sportsclub/fightclub/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('groups/', views.groups, name='groups'),
+    path('groups/<int:group_id>', views.group, name='group'),
 ]
--- sportsclub/fightclub/views.py
+++ sportsclub/fightclub/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Membership, UserMembership, Group
+import this
 
 def index(request):
     text = "Labas baigiamasais darbas"
-    context = {text, }
-    return render(request, 'fightclub/index.html')
+    context = {
+        'text': text,
+    }
+    return render(request, 'fightclub/index.html', context=context)
 
-# Create your views here.
+def about(request):
+    return render(request, 'fightclub/about.html')
+
+def groups(request):
+    groups = Group.objects.all()
+    context = {
+        'groups': groups,
+    }
+    return render(request, 'fightclub/groups.html', context=context)
+
+def group(request, group_id):
+    single_group = get_object_or_404(Group, pk=group_id)
+    return render(request, 'fightclub/group.html', {'group': single_group})
+
+
